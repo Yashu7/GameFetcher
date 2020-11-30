84fda78ae485360d123a18e5d417e3da8a0145a3
MieszkoMalawski <mieszko.malawski@miinto.com>
2020-11-30 21:09:47 +0100

    komentarz #2

--- APIapp/TwitchApiCalls.cs
+++ APIapp/TwitchApiCalls.cs
@@ -18,6 +18,7 @@ namespace APIapp
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             /** nie commituj nigdy secreta :) to jest tyko do developmentu - docelowo aplikacja powinna użyać np. user flow https://auth0.com/docs/authorization/which-oauth-2-0-flow-should-i-use **/
+            /** URL do twitcha można wynieść do jakiejś konfiguracji i wstrzykiwać przez konstruktor **/
             var response = await client.PostAsync("https://id.twitch.tv/oauth2/token?client_id=3yo2gt2qjjburcphl30wfyt0e64vxx&client_secret=hushpsrjlwb37log7vpjcux79mje3x&grant_type=client_credentials", null).ConfigureAwait(false);
             bearer = await response.Content.ReadAsAsync<TwitchAuth>().ConfigureAwait(false);
             return bearer;
