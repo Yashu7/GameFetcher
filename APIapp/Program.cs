804efe4b1f84d35764d68b69972aaa0a709a2d82
Jan <janmalawski2@gmail.com>
2020-12-15 00:33:35 +0100

    xaml changes

--- APIapp/Program.cs
+++ APIapp/Program.cs
@@ -1,10 +1,6 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +8,7 @@ namespace APIapp
 {
     class Program
     {
-        static void  Main(string[] args)
+         static void Main(string[] args)
         {
 
         }
